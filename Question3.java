55a1f7fb8a548a0c259df8503c2dca8686609225
DarkJoker521 <samuelkoh17@gmail.com>
2023-04-19 19:32:02 +0800

    19/4

--- Question3.java
+++ Question3.java
@@ -11,6 +11,8 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int a = in.nextInt();
+    System.out.println(a*a);
+    in.close();
   }
 }
--- Question4.java
+++ Question4.java
@@ -18,6 +18,24 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int n = in.nextInt();
+    in.close();
+
+    for (int i = n; i > 0; i--){
+      for (int j = 0; j < i; j++){
+        System.out.print("*");
+      }
+        System.out.println();
+    }
+    //__________________________________________________________
+    /*Alternative answer: 
+    for (int i = 0; i < n; i++){
+      for (int j = 0; j < (n-i); j++){
+        System.out.print("*");
+      }
+        System.out.println();
+    }
+   */
   }
+
 }
